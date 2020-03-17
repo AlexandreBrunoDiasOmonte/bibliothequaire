b10b5b0bd386e5f7ea79861444b3747d625ec1b0
AlexandreBrunoDiasOmonte <alexandrebruno.dias--omonte@orange.fr>
2020-03-17 17:40:06 +0100

    modification de la BDD + administration

--- KIOSQUE/admin.py
+++ KIOSQUE/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
+from .models import Auteur, Livre, Utilisateur, Editeur, Reservation, Genre
 
-# Register your models here.
+
+@admin.register(Livre)
+class LivreAdmin(admin.ModelAdmin):
+    list_filter = ['genre', 'auteur', 'editeur']
+    search_fields = ['titre', 'genre', 'auteur', 'editeur']
+    list_display = ['titre', 'auteur', 'genre', 'parution']
+    list_per_page = 10
+    ordering = ['titre']
+
+
+@admin.register(Utilisateur)
+class UtilisateurAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ['name', 'email']
+    list_per_page = 10
+    ordering = ['name']
+
+
+@admin.register(Auteur)
+class AuteurAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'userName']
+    list_display = ['name', 'userName']
+    list_per_page = 10
+    if 'userName' == "":
+        ordering = ["name"]
+    else:
+        ordering = ['userName']
+
+
+@admin.register(Editeur)
+class EditeurAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ['name', 'website', 'phone', 'email']
+    list_per_page = 10
+    ordering = ['name']
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    search_fields = ['utilisateur_id', 'reserved']
+    list_display = ['utilisateur_id', 'reserved', 'created_at']
+    list_per_page = 10
+    ordering = ['-created_at']
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    search_fields = ['genre']
+    list_display = ['genre']
+    ordering = ['genre']
+    list_per_page = 10
--- KIOSQUE/models.py
+++ KIOSQUE/models.py
@@ -2,41 +2,63 @@ from django.db import models
 
 
 class Auteur(models.Model):
-    name = models.CharField('Nom', max_length=25)
-    surname = models.CharField('Prénom', max_length=25)
+    name = models.CharField("Nom de l'auteur", max_length=50)
+    userName = models.CharField("Nom d'usage", max_length=50, blank=True)
     photo = models.URLField('Photo', blank=True)
-    birthDate = models.DateField('Date de naissance', blank=True)
-    deathDate = models.DateField('Date de décès', blank=True)
+
+    def __str__(self):
+        if self.userName == "":
+            return self.name
+        else:
+            return self.userName
 
 
 class Editeur(models.Model):
-    name = models.CharField('Nom', max_length=200)
+    name = models.CharField("Nom de l'éditeur", max_length=200)
     website = models.URLField('Site internet', max_length=200, blank=True)
-    phone = models.CharField('Téléphone', max_length=13, blank=True)
+    phone = models.CharField('Téléphone', max_length=18, blank=True)
     adresse = models.CharField('Adresse', max_length=200, blank=True)
     email = models.EmailField('Email', max_length=50, blank=True)
 
+    def __str__(self):
+        return self.name
 
-class Contact(models.Model):
+
+class Genre(models.Model):
+    genre = models.CharField('Genre du livre', max_length=100, blank=True)
+
+    def __str__(self):
+        return self.genre
+
+
+class Utilisateur(models.Model):
     name = models.CharField('Nom', max_length=200)
     email = models.EmailField('Email', max_length=100)
     photo = models.URLField('Photo', blank=True)
+    adresse = models.CharField('Adresse', max_length=200, blank=True)
+    phone = models.CharField('Téléphone', max_length=18, blank=True)
 
-
-class Book(models.Model):
-    titre = models.CharField('Titre du livre', max_length=200)
-    available = models.BooleanField('Disponneble ?', default=True)
-    parution = models.DateField('Date de parution')
-    nbPages = models.PositiveIntegerField('Nombre de pages')
-    nature = models.CharField('Nature du livre', max_length=200)
-    auteur = models.ForeignKey(Auteur, on_delete=models.CASCADE)
-    editeur = models.ManyToManyField(Editeur, related_name="books")
-    couverture = models.URLField('Image de courverture', blank=True)
-    resume = models.TextField('Résumé')
+    def __str__(self):
+        return self.name
 
 
 class Reservation(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     reserved = models.BooleanField('Réservé ?', default=False)
-    contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
-    book = models.OneToOneField(Book, on_delete=models.CASCADE)
+    utilisateur_id = models.ForeignKey(Utilisateur, on_delete=models.CASCADE)
+
+
+class Livre(models.Model):
+    titre = models.CharField('Titre du livre', max_length=200)
+    available = models.BooleanField('Disponneble ?', default=True)
+    parution = models.DateField('Date de parution')
+    nbPages = models.PositiveIntegerField('Nombre de pages', blank=True, null=True)
+    couverture = models.URLField('Image de courverture', blank=True)
+    resume = models.TextField('Résumé', blank=True)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    editeur = models.ManyToManyField(Editeur, related_name="livres")
+    auteur = models.ForeignKey(Auteur, on_delete=models.CASCADE)
+    Reservation_id = models.ForeignKey(Reservation, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titre
